fabe4f36a67f5f3d57c78cc962cf0b1085f5020d
Naeem Ayoub <nomi@imada.sdu.dk>
2021-05-31 20:18:11 +0200

    add model summary

--- run_main.py
+++ run_main.py
@@ -1,11 +1,10 @@
 import torch
 from models.fext_model.vgg_model import Vgg_Net
 from config.vgg_config import cfg
-
-
-vgg_layers = cfg.vgg.vgg19
+from pytorch_model_summary import summary
+vgg_layers = cfg.vgg.vgg16
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = Vgg_Net(in_ch=3, n_classes=2, vgg_model=vgg_layers).to(device)
-x = torch.randn(1, 3, 224, 244)
-print(model)
+x = torch.randn(1, 3, 224, 224)
+print(summary(model, x))
